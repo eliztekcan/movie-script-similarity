0e23b1e5c12c4fa3580f95df805c0ecfc5a0cd36
Metehan Kaya <kaya.metehank@gmail.com>
2018-11-30 15:07:07 +0300

    Added comments to TF-IDF

--- src/cpp/tf-idf/TF-IDF.cpp
+++ src/cpp/tf-idf/TF-IDF.cpp
@@ -24,18 +24,22 @@ double tfidf[MAX_NO_DOC][MAX_NO_TOKEN];
 double distL[MAX_NO_DOC][MAX_NO_DOC];
 double distCos[MAX_NO_DOC][MAX_NO_DOC];
 
-int docFreq[MAX_NO_TOKEN];
-int noAllTokens[MAX_NO_DOC];
-int charConvertedValue[MAX_CHAR];
-int cntToken[MAX_NO_DOC][MAX_NO_TOKEN];
+int docFreq[MAX_NO_TOKEN]; // number of docs that has a specific token at least once
+int noAllTokens[MAX_NO_DOC]; // number of all tokens in a specific doc
+int charConvertedValue[MAX_CHAR]; // uppercase -> lowercase etc.
+int cntToken[MAX_NO_DOC][MAX_NO_TOKEN]; // # of appearance of a token in a doc
 
 char inputFileNames[MAX_NO_DOC][MAX_FILE_NAME_SIZE];
 
-set<string>nonusedWords;
-map<string,int>hashToken;
-map<string,string>stemWords;
-string tokens[MAX_NO_TOKEN];
+set<string>nonusedWords; // stop words
+map<string,int>hashToken; // id of tokens
+map<string,string>stemWords; // hashing of words to original ones
 
+string tokens[MAX_NO_TOKEN]; // list of different words extracted from scripts
+
+// Map characters to other characters like uppercase -> lowercase
+// Also consider other characters as spaces
+// Dash '-' can be considered as '-' or space, not sure!
 void calcConvertChar() {
 	for( int i = 0 ; i < MAX_CHAR ; i++ )
 		charConvertedValue[i] = SPACE_VALUE;
@@ -44,6 +48,7 @@ void calcConvertChar() {
 	//charConvertedValue[DASH_VALUE] = DASH_VALUE;
 }
 
+// Map stemming words to originals (roots)
 void getStemmingWords() {
 	cin.clear();
 	freopen( "diffs.txt" , "r" , stdin );
@@ -65,6 +70,7 @@ void getStemmingWords() {
 	}
 }
 
+// Copy words in the text to stop-words set
 void getNonUsedWords() {
 	cin.clear();
 	freopen( "nonused.txt" , "r" , stdin );
@@ -73,6 +79,7 @@ void getNonUsedWords() {
 		nonusedWords.insert( word );
 }
 
+// Get names of script files that will be used during testing
 void getInputFileNames() {
 	cin.clear();
 	freopen( "inputFileNames.txt" , "r" , stdin );
@@ -85,14 +92,20 @@ void getInputFileNames() {
 	}
 }
 
+// Check if a word should be used in TF-IDF
+// Returns true if it should be used, false otherwise
 bool checkWord( string &word ) {
 	int len = word.length();
+	// All words with length <= 2 are stop words
 	if( len <= 2 )
 		return false;
+	// Check if given word is a stop word or not
 	if( nonusedWords.find( word ) != nonusedWords.end() )
 		return false;
 	return true;
 	/*
+	// A word may be represented as full of '-' characters
+	// May spend too much time unnecessarily
 	for( int i = 0 ; i < len ; i++ )
 		if( word[i] != '-' )
 			return true;
@@ -100,22 +113,30 @@ bool checkWord( string &word ) {
 	*/
 }
 
+// Calculation of TF-IDF
 void calcTFIDF() {
+	// STL Map iterators
 	map<string,int>::iterator mit_si;
 	map<string,string>::iterator mit_ss;
+	// Look for all script files
 	for( int fileId = 0 ; fileId < n ; fileId++ ) {
 		cin.clear();
 		freopen( inputFileNames[fileId] , "r" , stdin );
 		string line;
+		// Look for all lines of the file, file may consist of more than 1 line
 		while( getline( cin , line ) ) {
 			int length = line.length();
+			// Find words in the line, use 2 pointers (left & right) logic
 			for( int i = 0 , j = 0 ; i < length ; i = ++j ) {
 				int ci = charConvertedValue[ (int) line[i] ];
+				// Skip spaces
 				if( ci == SPACE_VALUE ) {
 					while( j+1 < length && charConvertedValue[ (int) line[j+1] ] == SPACE_VALUE )
 						j++;
 				}
+				// Currently, looking for a word
 				else {
+					// Get the next word
 					string word = "";
 					char c = (char) charConvertedValue[ (int) line[i] ];
 					word.pb( c );
@@ -123,19 +144,30 @@ void calcTFIDF() {
 						char c = (char) charConvertedValue[ (int) line[++j] ];
 						word.pb( c );
 					}
+					// Check if it's a stemming word that means its original is different
+					// If it's so, convert it to the original one
 					mit_ss = stemWords.find( word );
 					if( mit_ss != stemWords.end() )
 						word = (*mit_ss).se;
+					// Check if it's a step word or not
 					if( checkWord(word) ) {
+						// Check if the word is in the list of words
+						// that are extracted from script files up to now
 						int tokenId;
 						mit_si = hashToken.find( word );
+						// If the word's not in the list, add give next id to that word
 						if( mit_si == hashToken.end() ) {
 							hashToken[word] = tokenId = m++;
 							tokens[tokenId] = word;
 						}
+						// Otherwise, use the id of word that is given before
 						else
 							tokenId = (*mit_si).se;
+						// Increase # of tokens (words including the same ones) by 1
+						// It's needed to calculate tf in later steps
 						noAllTokens[fileId]++;
+						// If it's appearance of the word in the doc
+						// Then increase appearance of that token's id in different docs by 1
 						if( ++cntToken[fileId][tokenId] == 1 )
 							docFreq[tokenId]++;
 					}
@@ -143,11 +175,14 @@ void calcTFIDF() {
 			}
 		}
 	}
+	// Display all words in the list
 	printf( "# All words : %d\n" , m );
 	for( int i = 0 ; i < n ; i++ )
 		cout << inputFileNames[i] << " : " << noAllTokens[i] << endl;
+	// Calculate idf of the words in the list
 	for( int i = 0 ; i < m ; i++ )
 		idf[i] = log10( (double) n / docFreq[i] );
+	// Calculate tf, then tf-idf
 	for( int i = 0 ; i < n ; i++ )
 		for( int j = 0 ; j < m ; j++ ) {
 			tf[i][j] = (double) cntToken[i][j] / noAllTokens[i];
@@ -155,6 +190,7 @@ void calcTFIDF() {
 		}
 }
 
+// O(logN) implementation of x^y
 double calcPower( double value , int pwr ) {
 	if( pwr == 0 )
 		return 1;
@@ -165,12 +201,14 @@ double calcPower( double value , int pwr ) {
 	return res;
 }
 
+// Calculate L2 distance
 double calcDistLX( int docX , int docY , int dim ) {
 	double ans = 0;
-	// dim = inf
+	// If dim = inf, then store it as -1
 	if( dim == -1 )
 		for( int i = 0 ; i < m ; i++ )
 			ans = max( ans , fabs( tfidf[docX][i] - tfidf[docY][i] ) );
+	// Calculate squares of differences
 	else {
 		for( int i = 0 ; i < m ; i++ )
 			ans += calcPower( fabs( tfidf[docX][i] - tfidf[docY][i] ) , dim );
@@ -179,33 +217,44 @@ double calcDistLX( int docX , int docY , int dim ) {
 	return ans;
 }
 
+// Calculate cosine distance in degree
 double calcDistCos( int docX , int docY ) {
 	double multSum = 0 , lenX = 0 , lenY = 0;
 	for( int i = 0 ; i < m ; i++ ) {
+		// Sum up terms of the numerator
 		multSum += tfidf[docX][i] * tfidf[docY][i];
 		lenX += tfidf[docX][i] * tfidf[docX][i];
 		lenY += tfidf[docY][i] * tfidf[docY][i];
 	}
+	// Calculate length of vectors - terms of the denominator
 	lenX = sqrt( lenX );
 	lenY = sqrt( lenY );
+	// Calculate numerator / denominator
 	double cosVal = multSum / ( lenX * lenY );
+	// Get cosine distance in radian
 	double tetaVal = acos( cosVal );
+	// Get cosine distance in degree
 	double cosDist = tetaVal * 180.0 / M_PI;
 	return cosDist;
 }
 
+// Display different kinds of distances
 void calcAllDistances() {
+	// Calculate L2 distances
 	for( int i = 0 ; i < n ; i++ )
 		for( int j = i + 1 ; j < n ; j++ )
 			distL[i][j] = distL[j][i] = calcDistLX( i , j , 2 );
+	// Calculate Cos distances
 	for( int i = 0 ; i < n ; i++ )
 		for( int j = i + 1 ; j < n ; j++ )
 			distCos[i][j] = distCos[j][i] = calcDistCos( i , j );
+	// Display different kinds of distances
 	for( int i = 0 ; i < n ; i++ )
 		for( int j = i ; j < n ; j++ )
 			cout << i << " , " << j << " : " << "Cos = " << distCos[i][j] << " / " << "L2 = " << distL[i][j] << endl;
 }
 
+// A short test function code that can be run to check if the algo works correctly
 void test() {
 	n = 3;
 	m = 3;
@@ -217,11 +266,17 @@ void test() {
 
 int main() {
 	//test();
+	// 1st step: map characters
 	calcConvertChar();
+	// 2nd step: get stop words
 	getNonUsedWords();
+	// 3rd step: map words to original ones - stemming job
 	getStemmingWords();
+	// 4th step: get file names that will be used during test
 	getInputFileNames();
+	// 5th step: major step that calculates tf-idf
 	calcTFIDF();
+	// 6th step: calculate distances in different measures
 	calcAllDistances();
 	return 0;
 }
